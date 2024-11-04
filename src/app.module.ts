27b31c68352cda9caaae6a70870a9272fa8a3d30
Yuri Mancini <yurimancini@gmail.com>
2024-11-04 18:45:12 -0300

    config app module base and swagger connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,26 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { config } from 'dotenv';
 
+config();
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get('TYPEORM_HOST'),
+        port: configService.get('TYPEORM_PORT'),
+        username: configService.get('TYPEORM_USERNAME'),
+        password: configService.get('TYPEORM_PASSWORD'),
+        database: configService.get('TYPEORM_DATABASE'),
+        entities: [],
+        synchronize: false,
+        autoLoadEntities: true,
+      }),
+    }),
+  ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,32 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+const packageFile = require('../package.json');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const options = new DocumentBuilder()
+    .setTitle(`WeFit Form - ${process.env.NODE_ENV}`)
+    .setDescription('Back-end do teste relacionado ao form')
+    .setVersion(packageFile.version)
+    .addBearerAuth()
+    .addTag('Back-end')
+    .build();
+
+  app.useGlobalPipes(new ValidationPipe());
+  app.enableCors();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
+  const PORT = Number(process.env.PORT) || 3000;
+  await app.listen(PORT, '0.0.0.0');
+  console.log(
+    `${packageFile.name} is running on ${process.env.NODE_ENV} mode and is listening port : `,
+    PORT,
+  );
 }
 bootstrap();
